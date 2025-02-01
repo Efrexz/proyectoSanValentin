b2a6874611a49cf5511076d4c2fa287e6ba60c6b
Efrexz <zyzz_448@hotmail.com>
2025-02-01 18:01:33 -0500

    modificando las imagenes

--- src/App.jsx
+++ src/App.jsx
@@ -15,53 +15,53 @@ function App() {
   let random = [{
     id: 1,
     description: "Di si por favor",
-    img: "https://i.pinimg.com/originals/db/aa/c1/dbaac13f6278b91a15e480752b8a7242.gif"
+    img: "https://media.tenor.com/-2wfkYNfRtwAAAAj/heart.gif"
   },
   {
     id: 1,
-    description: "Piénsalo de nuevo.",
-    img: "https://i.pinimg.com/originals/77/6b/21/776b215bed3deeef47fd3aa657685a18.gif"
+    description: "Piénsalo de nuevo NOJODA.",
+    img: "https://i.ibb.co/LdNrztrf/Niomara.png"
   }
     ,
   {
     id: 2,
-    description: "Vamos, atrévete a decir que sí.",
-    img: "https://www.gifmaniacos.es/wp-content/uploads/2019/05/gatitos-kawaii-gifmaniacos.es-19.gif"
+    description: "Enserio??????",
+    img: "https://media.tenor.com/Wa8ncYRb5BQAAAAi/arreglemos-esto-con-dialogo.gif"
   },
   {
     id: 3,
-    description: "No tengas miedo, será genial.",
-    img: "https://i.pinimg.com/originals/e1/c3/88/e1c388133e0f998e25bb17c837b74a14.gif"
+    description: "Solo presiona el boton de SIIII COÑOO.",
+    img: "https://media.tenor.com/FWNI_0Kvz-kAAAAi/peep-knife-peep.gif"
   },
   {
     id: 4,
     description: "Confía en mí, será divertido.",
-    img: "https://media.tenor.com/Bn88VELdNI8AAAAi/peach-goma.gif"
+    img: " https://media1.tenor.com/m/MyeRzkzyZtcAAAAC/gato.gif"
   },
   {
     id: 5,
-    description: "No tengas dudas, te hará sonreír.",
-    img: "https://i.pinimg.com/originals/c6/b3/0d/c6b30d1a2dc178aeb92de63295d4ae64.gif"
+    description: "Me tas jodiendo? ",
+    img: "https://media.tenor.com/8InTyX-fzM8AAAAi/safelyendangered-safely-endangered.gif"
   },
   {
     id: 6,
-    description: "Te prometo que será inolvidable.",
-    img: "https://media.tenor.com/N2oqtqaB_G0AAAAi/peach-goma.gif"
+    description: "Creo que te equivocaste de BOTON",
+    img: "https://media1.tenor.com/m/xxGwVgDk4OMAAAAC/ojo-perro.gif"
   },
   {
     id: 7,
-    description: "No dejes que el miedo te detenga.",
-    img: "https://i.pinimg.com/originals/db/aa/c1/dbaac13f6278b91a15e480752b8a7242.gif"
+    description: "No te resistas, yo se que si quieres.",
+    img: "https://media.tenor.com/Sl0eZzsbjQUAAAAi/cat-cute-cat.gif"
   },
   {
     id: 8,
-    description: "Confía en el destino, nos está dando una señal.",
-    img: "https://media.tenor.com/cbEccaK9QxMAAAAi/peach-goma.gif"
+    description: "EFRAINSITO NO ESTARIA FELIZ.",
+    img: "https://media1.tenor.com/m/jLSQskeoWMMAAAAC/wtf-confused.gif"
   },
   {
     id: 9,
-    description: "Confía en mí.",
-    img: "https://i.pinimg.com/originals/db/aa/c1/dbaac13f6278b91a15e480752b8a7242.gif"
+    description: "Yo se que si quieres pajua.",
+    img: "https://media.tenor.com/OoZ8EJH3bNkAAAAi/licking-mirror-the-pet-collective.gif"
   },
   {
     id: 10,
@@ -72,9 +72,6 @@ function App() {
   const randomResponse = () => {
     let index = Math.floor(Math.random() * 11);
     console.log(random[index])
-    if (agrandar <= 500) {
-      setAgrandar(agrandar + 10)
-    }
     setRandomValor(random[index]);
   }
 
@@ -85,13 +82,13 @@ function App() {
 
 
   return (
-    <main id="canvas" className="fondo w-screen h-screen bg-no-repeat bg-cover flex items-center justify-center bg-center ">
+    <main id="canvas" className="fondo w-screen h-screen bg-no-repeat bg-cover flex items-center justify-center bg-center overflow-y-auto p-8 ">
       {
         !valueSi ? (
           <div className="p-5">
-            <h1 className="text-white font-bold text-5xl text-center">¿Quieres ser mi San Valentin?</h1>
+            <h1 className="text-white font-bold text-5xl text-center">¿Lesly quieres ser mi Novia?</h1>
             <img src={Object.keys(randomValor).length === 0 ?
-              "https://i.pinimg.com/originals/db/aa/c1/dbaac13f6278b91a15e480752b8a7242.gif" : randomValor.img} alt="San Valentin" className="mx-auto" width={400} height={400} />
+              "https://media.tenor.com/qT2BYFdeY0oAAAAi/cute-cat.gif" : randomValor.img} alt="San Valentin" className="mx-auto" width={400} height={400} />
             <div className="grid grid-cols-1 md:grid-cols-2 mt-10 gap-5 items-center">
               <button onClick={() => {
                 setValueSi(true)
@@ -116,10 +113,19 @@ function App() {
             </div>
           </div>
         ) : (
-          <div className="flex justify-center items-center flex-col space-y-10">
-            <h1 className="text-4xl text-white font-bold">Sabia que dirias que si ❤️!</h1>
-            <img src="https://i.pinimg.com/originals/9b/dc/c6/9bdcc6206c1d36a37149d31108c6bb41.gif" alt="" className="mx-auto" />
-            <span hidden>{document.title = 'Sabia que dirias que si ❤️!'}</span>
+          <div className="flex justify-center items-center flex-col space-y-10 p-8" >
+            <h1 className="text-5xl text-white font-bold pt-16">Sabia que dirias que si ❤️. <br />
+              Pronto vendra un Efrainsito!
+            </h1>
+            <div className="flex justify-center items-center space-x-5 flex-wrap">
+              <img src="https://media1.tenor.com/m/0Sh7u1lRsyEAAAAd/wedding-crasher-hro.gif" alt="" className="mx-auto w-80 h-80" />
+              <img src="https://media1.tenor.com/m/kqNHgt2ulKIAAAAd/maduro.gif" alt="" className="mx-auto w-80 h-80" />
+              <img src="https://media.tenor.com/5V7zXdWo4UEAAAAi/happy-el-diablo.gif" alt="" className="mx-auto w-80" />
+              <img src="https://media1.tenor.com/m/EmZ0N3llkAkAAAAC/cat-cats.gif" alt="" className="mx-auto w-80" />
+              <img src="https://media.tenor.com/3CFDk07jFSsAAAAi/cattime-dance.gif" alt="" className="mx-auto w-80 h-80" />
+              <img src="https://media1.tenor.com/m/PuwBIZUZ8QAAAAAd/gorilla.gif" alt="" className="mx-auto w-80 h-80" />
+            </div>
+
           </div>
         )
       }
